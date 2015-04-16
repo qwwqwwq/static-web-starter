c8ac5cdba77823cff8615668c66c35f24ee9aeb5
Jeff Quinn <jeff@nunahealth.com>
2015-04-15 18:09:26 -0700

    added time that updates once per second

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function theFunction() {
-    window.alert("You have clicked me, but nothing happen! - Alan");
+    var ele = document.getElementById("countdown");
+    ele.innerHTML = "count down is at: " + new Date();
+    console.log("count down is at: " + new Date());
 }
+
+function timer() {
+    window.setInterval(theFunction, 1000);
+}
+
+timer();
